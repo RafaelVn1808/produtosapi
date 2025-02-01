b8f9043758e133b5407eccc5369b20434f068af1
Rafael Vinicius dos Reis Almeida <rafaelalmeida1808@gmail.com>
2025-02-01 11:46:40 -0300

    Relizando o Hello World e Criando  o endPoint para recebimento de produtos

---
+++ src/main/java/io/github/RafaelVn1808/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,17 @@
+package io.github.RafaelVn1808.produtosapi.controller;
+
+import io.github.RafaelVn1808.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Indica que a classe vai receber requisições REST
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @PostMapping//enviar dados/Criar recursos no setor
+    public void salva(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+    }
+}
---
+++ src/test/java/io/github/RafaelVn1808/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.RafaelVn1808.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
