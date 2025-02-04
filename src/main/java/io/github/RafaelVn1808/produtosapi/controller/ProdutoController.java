46b79dabf00a0813885957326e7c304df82176ea
Rafael Vinicius dos Reis Almeida <rafaelalmeida1808@gmail.com>
2025-02-04 20:37:01 -0300

    implementando o método de Buscar produtos.

--- src/main/java/io/github/RafaelVn1808/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/RafaelVn1808/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import io.github.RafaelVn1808.produtosapi.model.Produto;
 import io.github.RafaelVn1808.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -43,5 +44,10 @@ public class ProdutoController {
         produto.setId(id);
         produtoRepository.save(produto);
 
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 }
--- src/main/java/io/github/RafaelVn1808/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/io/github/RafaelVn1808/produtosapi/repository/ProdutoRepository.java
@@ -3,5 +3,9 @@ package io.github.RafaelVn1808.produtosapi.repository;
 import io.github.RafaelVn1808.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
 }
